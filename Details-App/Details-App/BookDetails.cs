72bea873b4d43c5e8ccdf3c57656405e17af5fdf
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2024-04-23 18:53:04 +0100

    add Details console app

---
+++ Details-App/Details-App/BookDetails.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Details_App
+{
+    public class BookDetails
+    {
+
+        //Fields are just properties without get and set... They can be used to set the values of properties
+        private string _title;
+        private string _description;
+        private int _price;
+
+        //Constructor {With parameter} : The constructor is what allows you to create a class
+        //structure - Access Modifier, Contructor Name{which is the same name as the class name}
+        public BookDetails(string title, string description, int price) 
+        {
+            _title = title;
+            _description = description;
+            _price = price;
+        }
+
+        //Properties: These are like attributes
+        //Structure - Access Modifier, Datatype, Property {get; set;}
+        public string Title { get { return _title; } }
+        public string Description { get { return _description; } }
+        public int Price { get { return _price; } }
+
+
+        //Method
+        //Structure - Access Modifier, Datatype, Method-name
+        public string ReturnDetails()
+        {
+            return $"Title: {Title} \nDescription: {Description} \nPrice:{Price}";
+        }
+
+
+    }
+}
---
+++ Details-App/Details-App/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Details_App
+{
+    public class Program
+    {
+      
+        static void Main(string[] args)
+        {
+
+            //This recieves the details from the console
+            Console.WriteLine("Enter the book title");
+            var title = Console.ReadLine();
+            Console.WriteLine("Enter the book description");
+            var description = Console.ReadLine();
+            Console.WriteLine("Enter a price");
+            var price = int.Parse(Console.ReadLine());
+
+
+
+            var details = new BookDetails(title, description, price);
+            var endResult = details.ReturnDetails();
+            Console.WriteLine(endResult);
+
+        }
+    }
+}
