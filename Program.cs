d64ac9d63765e3634163816986724eda431a0659
sulaimanadegoke <sulaimanogundare@gmail.com>
2024-05-07 23:28:34 +0100

    assignmentarrayofstringsubmitted

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Arrayofstrings.SO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "toyota", "bmw", "ford", "kia" };
+            foreach (var car in cars) 
+            {
+                char lastCharacter = car[car.Length - 1];
+                char capitalizeLastCharacter = char.ToUpper(lastCharacter);
+             Console.WriteLine(car.Replace(lastCharacter, capitalizeLastCharacter ));
+            }
+        }
+    }
+}
